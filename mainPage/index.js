12d631cb7626916c43d19e0035c20eb76ec3b625
Your Name <ruslankurman13@gmail.com>
2023-11-28 17:35:33 +0200

    basket

--- mainPage/index.js
+++ mainPage/index.js
@@ -1,11 +1,11 @@
 'use strict'
 
-function toggleNested(element) {
-  var nestedList = element.querySelector('.nested');
-  nestedList.classList.toggle('active');
-  document.addEventListener('click', function (event) {
-    if (!element.contains(event.target)) {
-      nestedList.classList.remove('active');
+ function toggleNested(element) {
+      var nestedList = element.querySelector('.nested');
+      nestedList.classList.toggle('active');
+      document.addEventListener('click', function (event) {
+        if (!element.contains(event.target)) {
+          nestedList.classList.remove('active');
+        }
+      });
     }
-  });
-}
